d58b03278007fbc90ff3b3c52ebfd75e15c2504c
Rohan Tangri <rohan.tangri@trili.tech>
2024-06-14 14:43:15 +0100

    move pythAddress to ts script

--- contracts/Pyth.sol
+++ contracts/Pyth.sol
@@ -7,15 +7,13 @@ contract Pyth {
     IPyth pyth;
     bytes32 ethUsdPriceId;
  
-    constructor() {
-        // BNB Testnet: 0x5744Cbf430D99456a0A8771208b674F27f8EF0Fb
-        // Etherlink Testnet: 0x2880aB155794e7179c9eE2e38200202908C17B43
-        pyth = IPyth(address(0x5744Cbf430D99456a0A8771208b674F27f8EF0Fb));
+    constructor(address pythAddress) {
+        pyth = IPyth(address(pythAddress));
         ethUsdPriceId = bytes32(0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace);
     }
 
     function getEthUsdPrice() public view returns (int64 price, int32 expo, uint256 publishTime) {
-        PythStructs.Price memory priceStruct = pyth.getPrice(ethUsdPriceId);
+        PythStructs.Price memory priceStruct = pyth.getPriceUnsafe(ethUsdPriceId);
         return (priceStruct.price, priceStruct.expo, priceStruct.publishTime);
     }
 
--- scripts/Pyth.ts
+++ scripts/Pyth.ts
@@ -3,8 +3,11 @@ import { ethers } from 'hardhat';
 async function main() {
   // Deploying Pyth contract
   console.log("Deploying contract...")
+  // BNB Testnet: 0x5744Cbf430D99456a0A8771208b674F27f8EF0Fb
+  // Etherlink Testnet: 0x2880aB155794e7179c9eE2e38200202908C17B43
+  const pythAddress = "0x2880aB155794e7179c9eE2e38200202908C17B43"
   const PythFactory = await ethers.getContractFactory('Pyth')
-  const pythContract = await PythFactory.deploy()
+  const pythContract = await PythFactory.deploy(pythAddress)
   await pythContract.waitForDeployment()
   console.log("Contract deployed!! Address:", await pythContract.getAddress())
 
