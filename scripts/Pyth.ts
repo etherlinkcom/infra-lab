1b1b14c73ba560a0d6008d78b3d161838d499311
Rohan Tangri <rohan.tangri@trili.tech>
2024-06-14 14:56:20 +0100

    auto choose pythAddress

--- scripts/Pyth.ts
+++ scripts/Pyth.ts
@@ -1,11 +1,16 @@
-import { ethers } from 'hardhat';
+import { ethers, network } from 'hardhat';
 
 async function main() {
   // Deploying Pyth contract
   console.log("Deploying contract...")
-  // BNB Testnet: 0x5744Cbf430D99456a0A8771208b674F27f8EF0Fb
-  // Etherlink Testnet: 0x2880aB155794e7179c9eE2e38200202908C17B43
-  const pythAddress = "0x2880aB155794e7179c9eE2e38200202908C17B43"
+  let pythAddress: string
+  if (network.name === 'bscTestnet') {
+    pythAddress = "0x5744Cbf430D99456a0A8771208b674F27f8EF0Fb";
+  } else if (network.name === 'etherlinkTestnet') {
+    pythAddress = "0x2880aB155794e7179c9eE2e38200202908C17B43";
+  } else {
+    throw new Error(`Unsupported network: ${network.name}`);
+  }
   const PythFactory = await ethers.getContractFactory('Pyth')
   const pythContract = await PythFactory.deploy(pythAddress)
   await pythContract.waitForDeployment()
