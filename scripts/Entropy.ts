0f9697257fa0ac7c46d030d8601488e421b044cb
Rohan Tangri <rohan.tangri@trili.tech>
2024-06-17 17:16:36 +0100

    update entropy script

--- scripts/Entropy.ts
+++ scripts/Entropy.ts
@@ -1,14 +1,41 @@
 import { ethers, network } from 'hardhat';
 import web3 from 'web3';
+import { eth } from "web3";
 
 async function main() {
-  const entropyContract = await ethers.getContractAt("Entropy", "0xFDc8e8f99C19E2a40D3450952D4Dc5862BBB4dc5")
-
+  const entropyContract = await ethers.getContractAt("Entropy", "0x21031182cc8b3C23e10F655aB23c7D9D22E3B7d7")
 
   // Running getRandomNumber function
-  const randomNumber = web3.utils.randomHex(32);
-  const num = await entropyContract.getRandomNumber(randomNumber);
-  console.log("Entropy number:", num);
+  const randomNumber = web3.utils.randomHex(32)
+  const fee = await entropyContract.getFee() + BigInt(1)
+  const tx = await entropyContract.getRandomNumber(randomNumber, {value: fee.toString()})
+  const txReceipt = await tx.wait();
+
+  if(!txReceipt){
+    console.error("Transaction receipt is null");
+    return;
+  }
+
+  console.log("tx hash:", txReceipt.hash)
+
+  // Decode the logs
+  const eventSignature = web3.utils.keccak256("RandomNumber(uint64)");
+  const logs = txReceipt.logs.filter(log => log.topics[0] === eventSignature);
+
+  logs.forEach(log => {
+    const decodedLog = eth.abi.decodeLog(
+      [
+        {
+          type: 'uint64',
+          name: 'sequenceNumber'
+        }
+      ],
+      log.data,
+      log.topics.slice(1)
+    );
+    
+    console.log(`Random Number: ${decodedLog.sequenceNumber}`);
+  });
 }
 
 main()
